138ecb2038ec477a5de1b8b37ca629e98e2fda68
Matthew Diamant <matthewdiamant@gmail.com>
2020-11-30 11:06:38 -0800

    WIP two-knights

---
+++ introductory-problems/two-knights.py
@@ -0,0 +1,18 @@
+# input = int(input())
+input = 2
+print('')
+
+for n in range(input):
+    n += 1
+    print(n)
+    output = 0
+    for first_x in range(n):
+        for first_y in range(n):
+            for second_x in range(n):
+                for second_y in range(n):
+                    if ()
+                    if (first_x != second_x or first_y != second_y):
+                        print(f'{first_x},{first_y} {second_x},{second_y}')
+                        output += 1
+    print(output)
+    print('')
